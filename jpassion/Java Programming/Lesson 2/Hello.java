53e07a786ea7ec5e9d11d045c26af53c6629a5e3
Leandro Baena Torres <leandrobaena@gmail.com>
2020-08-14 10:20:43 -0500

    Lección 2 completada

--- jpassion/Java Programming/Lesson 2/Hello.java
+++ jpassion/Java Programming/Lesson 2/Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main( String[] args ){
-        System.out.println("Hola mundo");
+        System.out.println("Este es mi primer programa Java!");
     }
 }
---
+++ jpassion/Java Programming/Lesson 2/MyHelloProject2/src/main/java/com/leandrobaena/myhelloproject2/Hello.java
@@ -0,0 +1,13 @@
+package com.leandrobaena.myhelloproject2;
+
+/**
+ *
+ * @author Leandro Baena
+ */
+public class Hello {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello, this is the first line from MyHelloProject2");
+        System.out.println("This is the second line from the same project");
+    }
+}
