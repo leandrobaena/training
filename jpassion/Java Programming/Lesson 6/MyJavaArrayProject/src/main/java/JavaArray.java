73b39a72d66e566c8fc80a5c0e3b4765a58324fb
leandro <leandro@leandrobaena>
2020-08-26 10:09:31 -0500

    Lección 6

---
+++ jpassion/Java Programming/Lesson 6/MyJavaArrayProject/src/main/java/JavaArray.java
@@ -0,0 +1,21 @@
+
+public class JavaArray {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        // Declare and create int array whose size is 10
+        int[] ages = new int[10];
+
+        for (int i = 0; i < ages.length; i++) {
+            ages[i] = 100 + i;
+        }
+        // Display the value of each entry in the array
+        for (int i = 0; i < ages.length; i++) {
+            System.out.print(ages[i]);
+        }
+    }
+
+}
