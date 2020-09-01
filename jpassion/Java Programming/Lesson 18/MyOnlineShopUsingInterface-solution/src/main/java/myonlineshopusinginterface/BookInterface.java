c9906c4d06d8fbffb868774eae384c1b2a7da7bd
leandro <leandro@leandrobaena>
2020-09-01 13:50:01 -0500

    Lección 18

---
+++ jpassion/Java Programming/Lesson 18/MyOnlineShopUsingInterface-solution/src/main/java/myonlineshopusinginterface/BookInterface.java
@@ -0,0 +1,9 @@
+
+package myonlineshopusinginterface;
+
+public interface BookInterface {  
+    public String getPublisher();
+    public void setPublisher(String publisher);
+    public int getYearPublished();
+    public void setYearPublished(int yearPublished);
+}
