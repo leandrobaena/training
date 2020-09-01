360ba31dad47744dd22f88835c7d74604256e2fd
leandro <leandro@leandrobaena>
2020-09-01 18:18:12 -0500

    Lección 20

---
+++ jpassion/Java Programming/Lesson 20/HierarchyLogging/src/main/java/mypackage/LoggingMonitor.java
@@ -0,0 +1,10 @@
+package mypackage;
+
+public class LoggingMonitor {
+
+    public static void main(String[] args) {
+        ParentLogger pLogger = new ParentLogger();
+        ChildLogger cLogger = new ChildLogger();
+        cLogger.aMethod();
+    }
+}
---
+++ jpassion/Java Programming/Lesson 20/SimpleLoggingExample2/src/main/java/SimpleLoggginExample2.java
@@ -0,0 +1,33 @@
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class SimpleLoggginExample2 {
+    
+    // A Logger object is used to log messages for a specific system
+    // or application component. Loggers are normally named, using a
+    // hierarchical dot-separated namespace. Logger names can be arbitrary
+    // strings, but they should normally be based on the package name or
+    // class name of the logged component. In addition it is possible
+    // to create "anonymous" Loggers that are not stored in the Logger
+    // namespace.
+    private static Logger logger = Logger.getLogger("loggerdemo");
+    
+    public static void main(String[] args) {
+        
+        // Set logging level
+        logger.setLevel(Level.SEVERE);
+         
+        // Log a INFO tracing message
+        logger.info("doing stuff");
+        
+        try{
+            System.out.println(3/0);
+        } catch(Exception e){
+            logger.log(Level.SEVERE,"dividing by 0");
+        }
+        
+        logger.info("done");
+    }
+    
+}
